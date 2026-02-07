f9be53c6cb567a0b8b0f24923e02102510c02131
Janndhelle31 <125235356+Janndhelle31@users.noreply.github.com>
2026-02-07 22:29:33 +0800

    fixed

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,12 @@
 import axios from 'axios';
 window.axios = axios;
 
+// This is the fix - makes Axios use same protocol as current page
+window.axios.defaults.baseURL = window.location.origin;
+
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+// Optional: Add some debug logging
+if (import.meta.env.DEV) {
+    console.log('Axios configured with baseURL:', window.axios.defaults.baseURL);
+}
